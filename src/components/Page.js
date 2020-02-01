bc1f678264885c06ecc2e32acbb0bf84a8396d1d
Jennifer Oenning <oenningstangjennifer@gmail.com>
2020-01-31 21:08:21 -0300

    making responsive website

--- src/components/Page.js
+++ src/components/Page.js
@@ -27,13 +27,15 @@ function Page() {
     return (
         <div id="container" style={{maxHeight:950, overflowY:"hidden", scrollBehavior:"smooth"}}>
             <div style={{ height: pageSize }}>
-                <img onMouseOver={() => {
-                    document.getElementById("photo").style.transform = "scale(1.1)";
-                }} onMouseLeave={() => {
-                    document.getElementById("photo").style.transform = "scale(1)";
-                }} onClick={() => clickLogo()} id="photo" style={{ transition: "0.5s", cursor: "pointer", width: 200, height: 200, borderRadius: 100, marginLeft: 800, marginTop: 350 }}
-                    src="https://instagram.fjoi5-1.fna.fbcdn.net/v/t51.2885-15/e35/s1080x1080/83875171_191377635390287_2085185710885606317_n.jpg?_nc_ht=instagram.fjoi5-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=Wwr8Md5J4qwAX_Pxg27&oh=6653a48ca99b346892f053c649e545ef&oe=5EBFAAB8"></img>
-                <h1 style={{ marginLeft: 769, marginTop: 20, color: "pink" }} >Jennifer Oenning</h1>
+                <div style={{height:255, left:"50%", top:"50%", position:"absolute", marginTop:-125, marginLeft:-125, display:"grid"}}>
+                    <img onMouseOver={() => {
+                        document.getElementById("photo").style.transform = "scale(1.1)";
+                    }} onMouseLeave={() => {
+                        document.getElementById("photo").style.transform = "scale(1)";
+                    }} onClick={() => clickLogo()} id="photo" style={{ margin:"auto",transition: "0.5s", cursor: "pointer", width: 200, height: 200, borderRadius: 100 }}
+                        src="https://instagram.fjoi5-1.fna.fbcdn.net/v/t51.2885-15/e35/s1080x1080/83875171_191377635390287_2085185710885606317_n.jpg?_nc_ht=instagram.fjoi5-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=Wwr8Md5J4qwAX_Pxg27&oh=6653a48ca99b346892f053c649e545ef&oe=5EBFAAB8"></img>
+                    <h1 style={{ marginTop: 20, color: "pink" }} >Jennifer Oenning</h1>
+                </div>
             </div>
         </div>
     )
